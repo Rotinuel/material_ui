5d2940d4966734ebf39d9092d3c1141f9bd0b5cc
Rotinuel <emmanuelisagenius@gmail.com>
2023-03-21 13:35:24 +0100

    demo_typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import Typography  from '@mui/material/Typography'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>h1 Heading</Typography>
+        <Typography variant='h2'>h2 Heading</Typography>
+        <Typography variant='h3'>h3 Heading</Typography>
+        <Typography variant='h4'>h4 Heading</Typography>
+        <Typography variant='h5'>h5 Heading</Typography>
+        <Typography variant='h6'>h6 Heading</Typography>
+
+
+        <Typography variant='subtitle1'>subtitle 1</Typography>
+        <Typography variant='subtitle2'>subtitle 2</Typography>
+
+        <Typography variant='body1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet saepe ab, vero eius autem dignissimos illo inventore magni incidunt impedit fugit voluptas excepturi illum ipsa commodi eaque, quaerat rem suscipit.</Typography>
+        <Typography variant='body2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus nihil rem, consectetur illo est eius aut, hic, modi accusantium enim eos repellat! Harum culpa enim ipsam animi, nihil porro aperiam!</Typography>
+    </div>
+  )
+}
